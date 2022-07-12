e43cdc14254b1347ad823c73f197560361cc7d2e
Kubanskiy <kubanskiy80@bk.ru>
2022-07-12 09:14:14 +0300

    add new class

---
+++ common/helpers/HttpClientHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace common\helpers;
+
+use yii\httpclient\Client;
+use yii\httpclient\Exception;
+use common\models\Checking;
+
+class HttpClientHelper
+{
+    public static function runCheck($url, $repeat, $delay)
+    {
+
+        for ($i = 1; $i <= $repeat; $i++) {
+            try {
+                $client = new Client();
+                $response = $client->createRequest()
+                    ->setMethod('GET')
+                    ->setUrl($url)
+                    ->send();
+                if ($response) {
+                    $check = new Checking();
+                    $check->url = $url;
+                    $check->delay = $delay;
+                    $check->status = $response->getStatusCode();
+                    $check->attemp = $i;
+                    $check->save();
+                    echo \Yii::$app->session->setFlash('success', "Сайт " . $url . " успешно проверен");
+                }
+                if ($response->getStatusCode() < 399) {
+                    break;
+                } else {
+                    sleep($delay);
+                }
+            } catch (Exception $e) {
+                //echo \Yii::$app->session->setFlash('error', $e->getMessage());
+                echo \Yii::$app->session->setFlash('error', "Сайт " . $url . " не существует или заблокирован Органами");
+                continue;
+            }
+        }
+    }
+}
