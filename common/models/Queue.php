afcf47d1d26efaceea069577e7eee04b9adff171
Kubanskiy <kubanskiy80@bk.ru>
2022-07-19 14:54:08 +0300

    create model Quenue

---
+++ common/models/Queue.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%queue}}".
+ *
+ * @property int $id
+ * @property string $channel
+ * @property resource $job
+ * @property int $pushed_at
+ * @property int $ttr
+ * @property int $delay
+ * @property int|null $reserved_at
+ * @property int|null $attempt
+ * @property int|null $done_at
+ */
+class Queue extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%queue}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['channel', 'job', 'pushed_at', 'ttr'], 'required'],
+            [['job'], 'string'],
+            [['pushed_at', 'ttr', 'delay', 'reserved_at', 'attempt', 'done_at'], 'integer'],
+            [['channel'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'channel' => 'Channel',
+            'job' => 'Job',
+            'pushed_at' => 'Pushed At',
+            'ttr' => 'Ttr',
+            'delay' => 'Delay',
+            'reserved_at' => 'Reserved At',
+            'attempt' => 'Attempt',
+            'done_at' => 'Done At',
+        ];
+    }
+}
